22f8bfeeba0f24a1e47f15ccd3736b12e1a3eaff
andres <andres.silva.09@hotmail.com>
2021-10-13 17:20:56 -0300

    Probando  repositorio en hithub

---
+++ src/Controlador/Main.java
@@ -0,0 +1,45 @@
+
+package Controlador;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+   
+    /*public static void main (String[] args) {
+        // TODO code application logic here
+    }*/
+    
+    
+
+    @Override
+    public void start(Stage primaryStage) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+            FXMLLoader loader= new FXMLLoader();
+            loader.setLocation(Main.class.getResource("/Vista/EjemploVista.fxml"));
+            Pane ventana = (Pane) loader.load();
+            Scene scene = new Scene (ventana);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        
+        } catch(IOException e){
+            System.out.println("tratar error"+ e.getMessage());
+        
+        }
+    }
+    
+       public static void main(String[] args) {
+        // TODO code application logic here     
+        System.out.println("hola mundo desde Main_controlador");
+        launch(args);            
+    }
+    
+    
+}
